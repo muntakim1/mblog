dd3104220ce77f63e362d157e62e3ce93b0e7cea
Muntakimur Rahaman <32785930+muntakim1@users.noreply.github.com>
2019-12-29 20:05:35 +0600

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -118,5 +118,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
